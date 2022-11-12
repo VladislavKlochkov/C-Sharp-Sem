c541d0da1f48b49570c6463c2d74e19e350f4cbf
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-11-12 23:33:14 +0400

    added a solution of the Task 02

---
+++ Task02_Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая 
+// 1. на вход принимает два числа и 
+// 2. выдаёт, какое число большее, 
+// 3. а какое меньшее.
+
+Console.WriteLine("Введите два числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} большее, число {b} меньшее");
+}
+
+else if (a < b)
+{
+    Console.WriteLine($"Число {b} большее, число {a} меньшее");
+}
+
+else Console.WriteLine($"Число {a} равно числу {b}");
