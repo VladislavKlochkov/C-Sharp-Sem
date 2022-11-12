5b658cacd3bc50fcae764abd85267d7e64d08d7d
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-11-13 00:20:27 +0400

    added a solution of the Task 06

---
+++ Task06_Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} - является чётным");
+}
+else Console.WriteLine($"Число {number} - не является чётным");
