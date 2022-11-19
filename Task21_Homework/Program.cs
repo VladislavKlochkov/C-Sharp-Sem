598fd6e099a3bec8037324902b0aeffd6d4374b8
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-11-19 18:02:50 +0400

    added Homework Sem 3

---
+++ Task21_Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 21
+// Напишите программу, которая 
+// 1. принимает на вход координаты двух точек и 
+// 2. находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    return Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya)*(yb - ya) + (zb - za)*(zb - za));
+}
+
+double res = Math.Round(Distance(x1, y1, z1, x2, y2, z2), 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние между заданными точками в 3D пространстве: {res}");
---
+++ Task23_Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int n)
+{
+    int index = 1;
+    while (index <= n)
+    {
+        Console.WriteLine($"{index,5} | {Math.Pow(index, 3),5}");
+        index++;
+    }
+}
+
+if (number > 0) Cube(number);
+else Console.WriteLine("Введите число больше нуля");
