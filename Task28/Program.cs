9c13803be701bf2c01b1f323d4a65de79efc48fe
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-11-22 23:20:17 +0400

    added Classwork Sem 4

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// 1. принимает на вход число N и 
+// 2. выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i; // fact = fact * i;
+    }
+    return fact;
+}
+if (number > 0)
+{
+    int factorial = Factorial(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+}
+else Console.WriteLine("Требуется ввести натуральное число.");
---
+++ Task30/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array2 = new int[5] { 4, 6, 7, 9, 3 };
+// int[] array1 = { 1, 4, 5, 5, 8 };
+
+// 0  1  2  3  4  5  6  7
+// 0, 0, 0, 0, 0, 0, 0, 0
+
+// array[0] = 345;
+// array[1] = 44;
+// array[6] = 87;
+
+int[] array = new int[8];
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
+
+// int[] array = new int[8];
+//             void FillArray(int[] arr)
+//             {
+//                 Random rnd = new Random();
+//                 for (int i = 0; i < arr.Length; i++)
+//                 {
+//                     arr[i] = rnd.Next(0, 2);
+//                 }
+//             }
+//             void PrintArray(int[] arr)
+//             {
+//                 Console.Write("[");
+//                 for (int i = 0; i < arr.Length; i++)
+//                 {
+//                     Console.Write($"{arr[i]}, ");
+//                 }
+//                 Console.Write("]");
+//             }
+//             FillArray(array);
+//             PrintArray(array);
