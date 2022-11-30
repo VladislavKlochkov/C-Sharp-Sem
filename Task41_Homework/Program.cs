cf94428d1066abc500517b2d6b2fca6694ee6bc8
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-11-30 22:25:33 +0400

    added Homework Sem 6

---
+++ Task41_Homework/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: 
+// 1. Пользователь вводит с клавиатуры M чисел. 
+// 2. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите общее количество чисел:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите число под индексом {i}:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PositiveNumbers(int[] array)
+{
+    int positiveNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) positiveNumber++;
+    }
+    return positiveNumber;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+if (m > 0)
+{
+    int[] arr = CreateArray(m);
+    PrintArray(arr);
+    int res = PositiveNumbers(arr);
+    Console.WriteLine($"Количество чисел больше 0 -> {res} ");
+}
+else Console.WriteLine("Введите число больше нуля");
---
+++ Task43_Homework/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: 
+// 1. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// 2. значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите параметры первой прямой");
+Console.Write("b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите параметры второй прямой");
+Console.Write("b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double PointX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double PointY(double b1, double k1, double b2, double k2)
+{
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    return y;
+}
+
+if (k1 != k2)
+{
+    double resX = Math.Round(PointX(b1, k1, b2, k2), 2, MidpointRounding.ToZero);
+    double resY = Math.Round(PointY(b1, k1, b2, k2), 2, MidpointRounding.ToZero);
+    Console.WriteLine($"Прямые пересекаются в точке ({resX},{resY})");
+}
+else Console.WriteLine("Прямые параллельны");
