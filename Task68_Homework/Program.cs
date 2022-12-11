d19dd23556acac63ed5a7fce5f8510be097a0fab
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2022-12-11 16:44:39 +0400

    bugs fixed Task68_Homework

--- Task68_Homework/Program.cs
+++ Task68_Homework/Program.cs
@@ -10,9 +10,9 @@ int n = Convert.ToInt32(Console.ReadLine());
 int Ack(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (n == 0) return Ack(m - 1, 1);
-    else return Ack(m - 1, Ack(m, n - 1));
+    if (n == 0) return Ack(m - 1, 1);
+    return Ack(m - 1, Ack(m, n - 1));
 }
 
-int res = Ack(m,n);
+int res = Ack(m, n);
 Console.WriteLine($"{res}");
